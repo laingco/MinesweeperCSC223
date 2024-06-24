077768c94f4a5f19ff1563f17b763854e0cdb629
laingco <laingco@whs.school.nz>
2024-06-24 15:19:44 +1200

    Started adding comments.

--- Minesweeper/src/Minesweeper.java
+++ Minesweeper/src/Minesweeper.java
@@ -1,6 +1,6 @@
 
 /**
- * Version - 1.17
+ * Version - 1.18
  * This is a simple minesweeper game made in java.
  *
  * Author - Cooper Laing
@@ -13,73 +13,82 @@ import java.awt.event.*;
 class gui implements MouseListener {
     private static int SCREEN_WIDTH = 1050;
     private static int SCREEN_HEIGHT = 900;
-    private int difficulty = 2;
-    private int gridSize[] = { 8, 10, 14, 18, 20, 24 };
-    private int flagSize[] = { 10, 40, 99 };
-    private int easyGrid[][] = new int[gridSize[0]][gridSize[1]];
-    private int mediumGrid[][] = new int[gridSize[2]][gridSize[3]];
-    private int hardGrid[][] = new int[gridSize[4]][gridSize[5]];
+    private int difficulty = 2; //The overall difficulty variable which any difficulty dependent value uses
+    private int gridSize[] = { 8, 10, 14, 18, 20, 24 }; //Array to store grid sizes and uses the difficulty variable to retrieve correct size
+    private int flagSize[] = { 10, 40, 99 }; //The flag count for each difficulty
+    private int easyGrid[][] = new int[gridSize[0]][gridSize[1]]; //Array to store nearby mine counter for easy mode
+    private int mediumGrid[][] = new int[gridSize[2]][gridSize[3]]; //Array to store nearby mine counter for medium mode
+    private int hardGrid[][] = new int[gridSize[4]][gridSize[5]]; //Array to store nearby mine counter for hard mode
     private JFrame jframe = new JFrame("Minesweeper game"); // create JFrame objects
-    private JButton tiles[][] = new JButton[20][24]; // -1 = bomb | 0 = safe to click | 1-8 = no. of mines nearby
-    private boolean flagged[][] = new boolean[20][24];
-    private boolean bombs[][] = new boolean[20][24];
-    private boolean visible[][] = new boolean[20][24];
-    private int coloursInt = 1;
-    private int flags = 0;
-    private JLabel flagsLabel = new JLabel("Flags left: " + flags);
-    private boolean gameRunning = true;
+    private JButton tiles[][] = new JButton[20][24]; //The array that stores all of the clickable game tiles
+    private boolean flagged[][] = new boolean[20][24]; //Stores whether each tile is flagged or not
+    private boolean bombs[][] = new boolean[20][24]; //Stores whether each tile is a bomb or not
+    private boolean visible[][] = new boolean[20][24]; //Stores whether each tile is visible or not
+    private int coloursInt = 1; //Stores what colour scheme is active
+    private int flags = 0; //Stores how many flags are remaining
+    private JLabel flagsLabel = new JLabel("Flags left: " + flags); //Flag count label
+    private boolean gameRunning = true; //Stores whether the game is running or not
 
-    ImageIcon temp1 = new ImageIcon("MinesweeperCSC223/Minesweeper/assets/resetButtonImage.png");
-    Image image = temp1.getImage();
-    Image newimg = image.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
-    ImageIcon resetImage = new ImageIcon(newimg);
+    ImageIcon temp1 = new ImageIcon("MinesweeperCSC223/Minesweeper/assets/resetButtonImage.png"); //Defines the source image as an icon
+    Image image = temp1.getImage(); //Converts to an standard image for resizing
+    Image newimg = image.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH); //Resizes the image
+    ImageIcon resetImage = new ImageIcon(newimg); //Converts the image back to an image icon.
 
+    //See above
     ImageIcon temp2 = new ImageIcon("MinesweeperCSC223/Minesweeper/assets/flagImage.png");
     Image image2 = temp2.getImage();
     Image newimg2 = image2.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
     ImageIcon flagImage = new ImageIcon(newimg2);
 
+    //See above
     ImageIcon temp4 = new ImageIcon("MinesweeperCSC223/Minesweeper/assets/bombImage.png");
     Image image4 = temp4.getImage();
     Image newimg4 = image4.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
     ImageIcon bombImage = new ImageIcon(newimg4);
 
-    JButton reset = new JButton(resetImage);
-    JMenuItem easy = new JMenuItem("Easy");
-    JMenuItem medium = new JMenuItem("Medium");
-    JMenuItem hard = new JMenuItem("Hard");
-    JMenuItem white = new JMenuItem("White");
-    JMenuItem green = new JMenuItem("Green (default)");
-    JMenuItem blue = new JMenuItem("Blue");
-    JMenuItem blueAndRed = new JMenuItem("Blue and Red");
-    JMenuItem purpleAndGreen = new JMenuItem("Purple and Green");
-    int elapsedTime = 0;
-    JLabel time = new JLabel("Time: " + elapsedTime + "s");
-    int gameEndTime = 0;
-    boolean plantedMines = false;
+    JButton reset = new JButton(resetImage); //Reset button
+    JMenuItem easy = new JMenuItem("Easy"); //Easy button
+    JMenuItem medium = new JMenuItem("Medium"); //Medium button
+    JMenuItem hard = new JMenuItem("Hard"); //Hard button
+    JMenuItem white = new JMenuItem("White"); //White button
+    JMenuItem green = new JMenuItem("Green (default)"); //Green button
+    JMenuItem blue = new JMenuItem("Blue"); //Blue button
+    JMenuItem blueAndRed = new JMenuItem("Blue and Red"); //Blue and red button
+    JMenuItem purpleAndGreen = new JMenuItem("Purple and Green"); //Purple and green button
+    int elapsedTime = 0; //Stores how long the curren t game has been going
+    JLabel time = new JLabel("Time: " + elapsedTime + "s"); //Time label
+    boolean plantedMines = false; //Stores whether the mines hae been planted yet
 
-    Timer timer = new Timer(1000, new ActionListener() {
+    Timer timer = new Timer(1000, new ActionListener() { //Timer which is used for timing the game.
         public void actionPerformed(ActionEvent e) {
             updateTimeLabel();
         }
     });
 
-    public String difficultyPicker(int choice) {
-        if (choice == 0) {
-            switch (difficulty) {
-                case 1:
-                    return ("Easy");
-                case 2:
-                    return ("Medium");
-                case 3:
-                    return ("Hard");
-                default:
-                    return ("N/A");
-            }
-        } else {
-            return (null);
+    /*
+     * This method is used once in the guiMethod method.
+     * All this method does is take the golbal difficulty variable and 
+     * output the difficulty in string form for use in labels.
+     */
+
+    public String difficultyPicker() {
+        switch (difficulty) {
+            case 1:
+                return ("Easy");
+            case 2:
+                return ("Medium");
+            case 3:
+                return ("Hard");
+            default:
+                return ("N/A");
         }
     }
+
+    /* 
+     * This method is used when I need to access a difficulty sepcific grid.
+     * All this method does is take the golbal difficulty variable and 
+     * then outputs the appropriate grid based on it.
+    */
 
     public int[][] gridPicker() {
         switch (difficulty) {
@@ -94,7 +103,18 @@ class gui implements MouseListener {
         }
     }
 
+    /* 
+     * This method is used when you first start the game, 
+     * when you are changing difficulties and when you reset the game.
+     * This method starts with removing all of the mouse listeners to remove a buildup of lag if it is called multiple times.
+     * It then defines the two jpanels used in the game and sets up the jframe.
+     * It then sets up the score panel.
+     * Then it fills the tiles array with jbuttons and sets them all up appropriately.
+     * After that it then adds all of the j components for the game to their appropriately.
+    */
+
     public void guiMethod() {
+        //Remove previous listeners/panels
         jframe.getContentPane().removeAll();
         easy.removeMouseListener(this);
         medium.removeMouseListener(this);
@@ -106,38 +126,38 @@ class gui implements MouseListener {
         blueAndRed.removeMouseListener(this);
         purpleAndGreen.removeMouseListener(this);
 
+        //Sets up jpanels and jframe
         JPanel score = new JPanel();
         JPanel playArea = new JPanel();
-        playArea.setLayout(new GridLayout(gridSize[2 * difficulty - 2], gridSize[2 * difficulty - 1]));
+        playArea.setLayout(new GridLayout(gridSize[2 * difficulty - 2], gridSize[2 * difficulty - 1])); //Sets up a gridlayout for the main play area
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setSize(SCREEN_WIDTH, SCREEN_HEIGHT); // set size of GUI screen
+        jframe.setSize(SCREEN_WIDTH, SCREEN_HEIGHT); //Set size of GUI screen
         jframe.setVisible(true);
 
-        elapsedTime = 0;
-        time.setText("Time: " + elapsedTime + "s");
-        JMenuBar menu = new JMenuBar();
-        JMenu difficultyDropdown = new JMenu("Difficulty");
-        JMenu colours = new JMenu("Colours");
-        JLabel difficultyLabel = new JLabel("Difficulty: " + difficultyPicker(0));
+        //Sets up labels
+        elapsedTime = 0; //Resets the timer count
+        time.setText("Time: " + elapsedTime + "s"); //Sets the timer text
+        JMenuBar menu = new JMenuBar(); //Defines the menu bar
+        JMenu difficultyDropdown = new JMenu("Difficulty"); //Adds difficulty menu
+        JMenu colours = new JMenu("Colours"); //Adds colour menu
+        JLabel difficultyLabel = new JLabel("Difficulty: " + difficultyPicker()); //Sets the difficulty label
+        flags = flagSize[difficulty - 1]; //Sets the flag count
+        flagsLabel.setText("Flags left: " + flags); //Updated flag label
 
-        flags = flagSize[difficulty - 1];
-        flagsLabel.setText("Flags left: " + flags);
-
-        // JButton tiles[][] = new
-        // JButton[gridSize[2*difficulty-2]][gridSize[2*difficulty-1]];
+        //Fills tiles array
         for (int y = 0; y < gridSize[2 * difficulty - 2]; y++) {
             for (int x = 0; x < gridSize[2 * difficulty - 1]; x++) {
                 tiles[y][x] = new JButton();
-                tiles[y][x].addMouseListener(this);
-                tiles[y][x].setFont(new Font("Serif", Font.PLAIN, 20));
-                tiles[y][x].setOpaque(true);
-                tiles[y][x].setBorderPainted(false);
-                tiles[y][x].setMargin(new Insets(0, 0, 0, 0));
+                tiles[y][x].addMouseListener(this); //Makes each tile listen for a mouse click
+                tiles[y][x].setFont(new Font("Serif", Font.PLAIN, 20)); 
+                tiles[y][x].setBorderPainted(false); //Removes button border
+                tiles[y][x].setMargin(new Insets(0, 0, 0, 0)); //Allows for larger text
                 playArea.add(tiles[y][x]);
             }
         }
-        drawPattern();
+        drawPattern(); //Correctly sets colour scheme
 
+        //Adds components and their listeners
         menu.add(difficultyDropdown);
         menu.add(colours);
         difficultyDropdown.add(easy);
@@ -157,29 +177,28 @@ class gui implements MouseListener {
         blue.addMouseListener(this);
         blueAndRed.addMouseListener(this);
         purpleAndGreen.addMouseListener(this);
-
         score.add(reset);
         score.add(flagsLabel);
         score.add(time);
         score.add(menu);
         score.add(difficultyLabel);
 
+        //Assign jpanel positions and set visible
         jframe.getContentPane().add(BorderLayout.NORTH, score);
         jframe.getContentPane().add(BorderLayout.CENTER, playArea);
         jframe.setVisible(true);
     }
 
+    /*
+     * This method is used when you click or change colour schemes.
+     * This method updates the tiles and colour schemes when called.
+     */
+
     public void updateScreen() {
         for (int y = 0; y < gridSize[2 * difficulty - 2]; y++) {
             for (int x = 0; x < gridSize[2 * difficulty - 1]; x++) {
-                int currentGrid[][] = new int[gridSize[2 * difficulty - 2]][gridSize[2 * difficulty - 1]];
-                currentGrid = gridPicker();
-                /*
-                 * if (currentGrid[y][x] == 0 && visible[y][x]) {
-                 * tiles[y][x].setIcon(uncoveredButton);
-                 * tiles[y][x].setFont(new Font("Serif", Font.PLAIN, 0));
-                 * } else
-                 */if (flagged[y][x]) {
+                int currentGrid[][] = gridPicker();
+                if (flagged[y][x]) {
                     tiles[y][x].setIcon(flagImage);
                     tiles[y][x].setFont(new Font("Serif", Font.PLAIN, 0));
                 } else if (currentGrid[y][x] > 0 && visible[y][x]) {
@@ -194,7 +213,16 @@ class gui implements MouseListener {
         System.out.println("Screen updated");
     }
 
+    /*
+     * This method is called when you first start the game and when you update the screen.
+     * In this method it defines all of the nessiscary colours for the game.
+     * It then loops through the grid first checking whether the tile is visible and 
+     * then if it isn't visible then in a checkered pattern, colouring two colours determined by the colorsInt variable.
+     * If the tile is visible it does the same process with a different two colours also based off of the colorsInt variable.
+     */
+
     public void drawPattern() {
+        //Define the required colours
         Color greenLight = new Color(167, 217, 72);
         Color greenDark = new Color(142, 204, 57);
         Color greenDarkBackground = new Color(215, 184, 153);
@@ -218,10 +246,11 @@ class gui implements MouseListener {
 
         for (int y = 0; y < gridSize[2 * difficulty - 2]; y++) {
             for (int x = 0; x < gridSize[2 * difficulty - 1]; x++) {
+                //Check for covered cells
                 if (!visible[y][x]) {
                     if (y % 2 == 0) {
                         if (x % 2 == 0) {
-                            switch (coloursInt) {
+                            switch (coloursInt) { //Pick a colour using the colorsInt variable
                                 case 0:
                                     tiles[y][x].setBackground(whiteLight);
                                     break;
@@ -403,10 +432,21 @@ class gui implements MouseListener {
         }
     }
 
+    /*
+     * This method is called every second because of the timer earlier defined.
+     * All this method does is update the time count and then the time label.
+     */
+
     public void updateTimeLabel() {
         elapsedTime++;
         time.setText("Time: " + elapsedTime + "s");
     }
+
+    /*
+     * This method is called once per game when the player clicks and the minesPlanted variable is false and the gameRunning variable is true.
+     * In this method It first plants the mines in random positions on the board whilst not planting any withing the 3x3 around the player click.
+     * It then resets the temporary nearbyMines array 
+     */
 
     public void plantMines(int xx, int yy) {
         int z = 0;
